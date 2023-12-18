ae248871c2701491061bec7817f56c49773151bc
EFE ERDOĞMUŞ <efe.erdogmus@ug.bilkent.edu.tr>
2023-12-18 19:46:47 +0300

    getting date and time

--- AquaWise/src/main/java/pictures/CurrentDateAndTime.java
+++ AquaWise/src/main/java/pictures/CurrentDateAndTime.java
@@ -1,9 +1,22 @@
+package pictures;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;  
 public class CurrentDateAndTime {  
+
+  int[] upcomingActivities = new int[3];
+  int[] pastActivities = new int[3]; 
+  static DateTimeFormatter dtf;
+
   public static void main(String[] args) {  
-   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-   LocalDateTime now = LocalDateTime.now();
-   System.out.println(dtf.format(now));
+    getCurrentDateAndTime();
   }  
-}  
+  static DateTimeFormatter getCurrentDateAndTime(){
+    dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println(dtf.format(now));
+
+    return dtf;
+
+  }
+
+}   
